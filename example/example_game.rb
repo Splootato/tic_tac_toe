e4f64b185013f55e817c4199f1aa30a138444d60
Michael Spezialetti <mike.a.spez@gmail.com>
2020-10-20 13:18:10 +0100

    Created Player name inputs

--- example/example_game.rb
+++ example/example_game.rb
@@ -2,7 +2,12 @@ root = File.expand_path("../", File.dirname(__FILE__))
 require "#{root}/lib/tic_tac_toe.rb"
 
 puts "Welcome to Tic Tac Toe"
-Mike = TicTacToe::Player.new({color: "X", name: "Mike"})
-Paloosh = TicTacToe::Player.new({color: "O", name: "Paloosh"})
-players = [Mike, Paloosh]
+puts "Player 1 enter your name."
+name1 = gets.chomp
+puts "Player 2 enter your name."
+name2 = gets.chomp
+
+Player1 = TicTacToe::Player.new({color: "X", name: name1 })
+Player2 = TicTacToe::Player.new({color: "O", name: name2 })
+players = [Player1, Player2]
 TicTacToe::Game.new(players).play
